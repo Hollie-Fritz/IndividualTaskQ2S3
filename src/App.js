bd1cef096c756a6c4beae1e524015d7bf42b6bf2
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-31 15:00:48 -0800

    remove header links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import ResInfo from './ResInfo';
-import ResMenu from './ResMenu';
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import ResInfo from "./ResInfo";
+import ResMenu from "./ResMenu";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (<BrowserRouter>
-<nav>
-  <div className="container mt-4">
-  <div class="p-3 mb-2 bg-gradient-dark text-white">
-    <Link to="/">ResInfo</Link>
-  
-    <Link to="/ResMenu/">ResMenu</Link>
-    </div>
-  </div>
-</nav>
-<Routes>
-  <Route path="/" element={<ResInfo />} />
-  <Route path="/ResMenu/" element={<ResMenu />} />
-</Routes>
-</BrowserRouter>)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ResInfo />} />
+        <Route path="/ResMenu/" element={<ResMenu />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
